7f6326bcdcf1ab184cee530f42532c43627a51fd
unknown <qaisprawirayudha@gmail.com>
2025-07-29 10:55:25 +0700

    Menambahkan Logo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,14 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
+   app: {
+    head: {
+      title: 'Projects',
+      link: [
+        { rel: 'icon', type: 'image/png/jpg', href: '/Zesty.jpg' }
+      ]
+    }
+  },
   vite: {
     plugins: [
       tailwindcss(),
